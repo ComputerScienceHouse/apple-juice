2fc9223f89ad15eea24837ecfbba6168c1882fb2
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2025-09-30 01:05:37 -0400

    Initial Commit

---
+++ CSH Drink/CSH_DrinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSH_DrinkApp.swift
+//  CSH Drink
+//
+//  Created by Campbell Bagley on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct CSH_DrinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
